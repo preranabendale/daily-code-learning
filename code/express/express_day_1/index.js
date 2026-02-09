03c6fac21845c76dc08ca612f33a2ac3fa02cf8e
Prerana Bendale <preranabendale1@gmail.com>
2026-02-09 17:25:03 +0530

    express_day_1

---
+++ code/express/express_day_1/index.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const app=express()
+app.get('/',(req,res)=>{
+    res.send("<h1>Welcome to express.js </h1>")
+})
+const PORT=3000
+const HOST='127.0.0.1'
+app.listen(PORT,HOST,()=>{
+    console.log(`Server runing up...http://${HOST}:${PORT}`)
+})
